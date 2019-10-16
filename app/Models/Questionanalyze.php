337a89b953497c2744fcef13da03dd65b977a9a3
IS1-HAMAMOTO\hamamoto <hamayuu686@gmail.com>
2019-10-16 10:54:06 +0900

    返り値の型宣言 Questionanalyze.php

--- app/Models/Questionanalyze.php
+++ app/Models/Questionanalyze.php
@@ -7,12 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Questionanalyze extends Model
 {
     protected $table = 'questionanalyze';
-
-    public function getMaxAnswerId(){
+    public function getMaxAnswerId() :int
+    {
         return intval(\DB::table($this->table)->max("answer_id"));
     }
     
-    public function saveList($dataList){
+    public function saveList($dataList) :void
+    {
         \DB::table($this->table)->insert($dataList);
     }
 }
