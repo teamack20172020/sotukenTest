423b72d39a6c94702f7c6f0a0930d5d1dce636f7
yamamoto <yamashu0506@gmail.com>
2019-10-29 15:32:56 +0900

    SQLエラーの修正

--- app/Models/Questionpoint.php
+++ app/Models/Questionpoint.php
@@ -13,20 +13,21 @@ class Questionpoint extends Model
     {
         $items = \DB::table($this->table)
             ->select(\DB::raw('objective_id , sum(point) as sum_point'))
-            ->where(function ($query) {
+            ->where(function ($query) use($answerList){
                 for($i=0;$i<count($answerList);$i++){
                     $question_id = intval(substr($answerList[$i],0,4));
-                    $answerFlg = intval(substr($answerList[$i],-1,1));
-                    $query->orWhere(function ($query_sub) {
+                	$answerFlg = intval(substr($answerList[$i],-1,1));
+                    $query->orWhere(function ($query_sub)  use($question_id,$answerFlg){
+
                         $query_sub->where('question_id', $question_id)->where('answerfig', $answerFlg);
                     });
                 }
             })
             ->groupBy('objective_id')
-            ->having('sum_point', '>', 0)
-            ->latest()
+            ->latest('sum_point')
+            ->limit(3)
             ->get();
-    
+            
         return $items;
     }
 }
