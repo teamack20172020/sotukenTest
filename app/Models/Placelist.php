394fe0388e3f285fd8d87d2a45d76d76eac65659
Shinkawa <teamack20172020@gmail.com>
2019-10-30 10:06:01 +0900

    コメントの追記

--- app/Models/Placelist.php
+++ app/Models/Placelist.php
@@ -4,21 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// 目的地リストテーブルへの操作
 class Placelist extends Model
 {
     protected $table = 'placelist';
-    //県コード・目的別に取得
+
+    /**
+     * 目的: 目的地リストを県コード・目的別に取得
+     * 引数: objectId　目的ID
+     * 　　　areaId　地域ID
+     **/
     public function findByAreaIdAndObjectId($objectId,$areaId) :array
     {
         $items = \DB::table($this->table)->where('objective_id',$objectId)->where('area_id',$areaId)->get()->toArray();
         return $items;
     }
 
+    /**
+     * 目的: 目的地リストを保存
+     * 引数: dataList　目的地リスト
+     **/
     public function saveList($dataList) :void
     {
         \DB::table($this->table)->insert($dataList);
     }
 
+    /**
+     * 目的: 目的地リストを地域ID別に削除
+     * 引数: areaId　地域ID
+     **/
     public function deleteByAreaId($areaId) :void
     {
         \DB::table($this->table)->where('area_id',$areaId)->delete();
