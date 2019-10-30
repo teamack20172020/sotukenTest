3bbcca858224dafc287374a0bae05fdd72dc3e31
Shinkawa <teamack20172020@gmail.com>
2019-10-30 10:08:44 +0900

    コメントの追記、不要なメソッドの削除

--- app/Models/Placekeyword.php
+++ app/Models/Placekeyword.php
@@ -4,18 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//　目的地リスト検索用キーワードテーブルへの操作
 class Placekeyword extends Model
 {
     protected $table = 'placekeyword';
-    //県コード別に取得
+    
+    /**
+     * 目的: 目的地リスト検索用キーワードリストを取得
+     * 引数: areaId　地域ID
+     **/
     public function findByAreaId($areaId) :array
     {
         $items = \DB::table($this->table)->where('area_id',$areaId)->get()->toArray();
         return $items;
-    }
-
-    public function saveList($dataList) :void
-    {
-        \DB::table($this->table)->insert($dataList);
     }
 }
