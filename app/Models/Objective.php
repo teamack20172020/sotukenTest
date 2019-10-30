433634b06efa73f3f1ea0a535268d9409812feb1
Shinkawa <teamack20172020@gmail.com>
2019-10-30 10:09:45 +0900

    コメントの追記

--- app/Models/Objective.php
+++ app/Models/Objective.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+//目的テーブルへの操作
 class Objective extends Model
 {
+    // テーブル名
     protected $table = 'objective';
+
+    /**
+     * 目的: 目的リストを取得
+     * 引数: なし
+     **/
     public function getAll() :array
     {
         $items = \DB::table($this->table)->get()->toArray();
