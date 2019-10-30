b9f0eb6129c3f63696d286847f8d230a7df9bd8a
Shinkawa <teamack20172020@gmail.com>
2019-10-30 10:03:19 +0900

    コメントの追記

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// 質問テーブルへの操作
 class Question extends Model
 {
     protected $table = 'question';
+
+    /**
+     * 目的: 質問リストの取得
+     * 引数: なし
+     **/
     public function getAll() :array
     {
         $items = \DB::table($this->table)->get()->toArray();
