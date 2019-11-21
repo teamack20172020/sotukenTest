350f311c50ab1f53d4b9bf9a59e71a28512d5054
teamack20172020 <teamack20172020@gmail.com>
2019-11-21 15:59:10 +0900

    コメントの追記

--- config/api.php
+++ config/api.php
@@ -15,14 +15,17 @@ return [
 
     // GoogleApiに関する設定
     'GoogleApi' => [
+        // テキスト検索
         'place_text' => [
             'url' => 'https://maps.googleapis.com/maps/api/place/textsearch/',
             'key' => 'AIzaSyCSaGHq03_pZW5_xZEXeiJ-zTfxY2AAo7M',
         ],
+        // 詳細検索(リファレンスIDをキーに指定)
         'place_detail'=> [
             'url' => 'https://maps.googleapis.com/maps/api/place/details/',
             'key' => 'AIzaSyCSaGHq03_pZW5_xZEXeiJ-zTfxY2AAo7M', 
         ],
+        // ルート検索
         'directions' => [
             'url' => 'https://maps.googleapis.com/maps/api/directions/',
             'key' => 'AIzaSyBezdwuYFibJdo7TIjPJb_dbyq7Wi-vhfg', 
@@ -32,15 +35,18 @@ return [
     //　DBに関する設定
     'database' =>[
         'master' => [
+            // 地域に関する情報（47都道府県）
             'area' => [
                 // マスタ区分
                 'kbn' => '1',
                 //全国
                 'all' => '48',
             ],
+            // 目的別の重み係数
             'objectiv_point' => [
                 'kbn' => '2',
             ],
+            // 場所タイプ別の滞在時間
             'place_type' =>[
                 'kbn' => '3',
             ],
